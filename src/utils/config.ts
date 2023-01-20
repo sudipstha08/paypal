2ae51be04a7556684bdbfdd0ac401fce8d29d45a
sudiptha08 <sudiptha08@gmail.com>
2023-01-20 14:30:56 +0545

    feat: add paypal

---
+++ src/utils/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  paypal: {
+    clientID: process.env.NEXT_PUBLIC_CLIENT_ID,
+    clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+  },
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './api'
 export { theme } from './theme'
+export * from './config'
