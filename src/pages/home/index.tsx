3aaf3e67d53e1603d1b432f5892a2ed7c1139f09
sudiptha08 <sudiptha08@gmail.com>
2023-01-24 17:33:43 +0545

    chore: update envs

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -32,10 +32,11 @@ const HomePage = () => {
             createOrder: function (data, actions) {
               // Set up the transaction
               return actions.order.create({
+                
                 purchase_units: [
                   {
                     amount: {
-                      value: '0.01',
+                      value: '10',
                     },
                   },
                 ],
